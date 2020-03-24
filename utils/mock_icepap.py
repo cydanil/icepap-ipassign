9ae4d9660645299e447c682e8294a5aad764d225
Cyril Danilevski <cydanil@gmail.com>
2020-03-24 14:14:22 +0100

    Remove target_count references from mock_icepap

--- utils/mock_icepap.py
+++ utils/mock_icepap.py
@@ -2,31 +2,28 @@ import socket
 import struct
 
 from ipassign import (acknowledgements, Acknowledgement,
-                      commands, Configuration, Message, MULTICAST_ADDR,
-                      MULTICAST_PORT)
-
-cosmos = "172.24.155.154"
-ip = cosmos
+                      commands, Configuration, MAX_PACKET_LENGTH,
+                      Message, MULTICAST_ADDR, MULTICAST_PORT)
 
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.IPPROTO_IP,
                 socket.IP_MULTICAST_IF,
-                socket.inet_aton(ip))
+                socket.inet_aton('0.0.0.0'))
 
 # Tell the operating system to add the socket to
 # the multicast group on all interfaces.
 group = socket.inet_aton(MULTICAST_ADDR)
 mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-mreq = socket.inet_aton(MULTICAST_ADDR) + socket.inet_aton(ip)
+mreq = socket.inet_aton(MULTICAST_ADDR) + socket.inet_aton('0.0.0.0')
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 # Bind to the server address
 sock.bind(('', MULTICAST_PORT))
 
 # Create a mock configuration to send ipassign
-mac = '00:0B:AD:C0:FF:EE'
+mac = '00:0b:ad:c0:ff:ee'
 config = Configuration(target_id=mac,
                        ip='172.24.155.105',
                        bc='172.24.155.25',
@@ -36,34 +33,37 @@ config = Configuration(target_id=mac,
                        hostname="hi_mom")
 
 message = Message(source=mac,
-                  target_id=1,
                   packet_number=0,
                   command=commands.SEND_CONFIG,
-                  payload=config,
-                  destination='00:22:19:06:bf:58')
+                  payload=config)
 
 while True:
     print('\nwaiting to receive message')
-    data, address = sock.recvfrom(1024)
+    data, address = sock.recvfrom(MAX_PACKET_LENGTH)
     m = Message.from_bytes(data)
-    print(m)
+    if m.source == mac:
+        print('--- Our reply:')
+        print('            ', end='')
+        print('\n            '.join([line for line in str(m).split('\n')]))
+        print('--------------')
+    else:
+        print(m)
 
     if m.command is commands.REQUEST_CONFIG:
+        message.destination = m.source
         sock.sendto(message.to_bytes(), (MULTICAST_ADDR, MULTICAST_PORT))
         message.packet_number += 1
 
-    if (m.command is commands.UPDATE_CONFIG and
-       m.dest == "00:0b:ad:c0:ff:ee"):
+    if m.command is commands.UPDATE_CONFIG and m.destination == mac:
         message.payload = m.payload
 
         if not m.payload.reboot:  # ipassign only sends config. payloads
             ack = Acknowledgement(m.packet_number,
                                   code=acknowledgements.OK)
             ack_msg = Message(source=mac,
-                              target_id=1,
                               command=commands.UPDATE_CONFIG_ACK,
                               payload=ack,
-                              destination='00:22:19:06:BF:58')
+                              destination=m.source)
             sock.sendto(ack_msg.to_bytes(),
                         (MULTICAST_ADDR, MULTICAST_PORT))
 
