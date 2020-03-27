4b8a5a875e8111862275df0df5acb58fd302bee9
Cyril Danilevski <cydanil@gmail.com>
2020-03-27 16:43:31 +0100

    Mock icepap: get mac address from argv or generate random

--- utils/mock_icepap.py
+++ utils/mock_icepap.py
@@ -1,5 +1,7 @@
+import random
 import socket
 import struct
+import sys
 
 from ipassign import (acknowledgements, Acknowledgement,
                       commands, Configuration, MAX_PACKET_LENGTH,
@@ -22,8 +24,15 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 # Bind to the server address
 sock.bind(('', MULTICAST_PORT))
 
+# Get a mac address for this session
+if len(sys.argv) == 3:
+    mac = sys.argv[-1]
+else:
+    mac = ':'.join([hex(random.randint(1, 255))[2:].zfill(2) for
+                    _ in range(6)])
+print(f'Working with {mac}')
+
 # Create a mock configuration to send ipassign
-mac = '00:0b:ad:c0:ff:ee'
 config = Configuration(target_id=mac,
                        ip='172.24.155.105',
                        bc='172.24.155.25',
