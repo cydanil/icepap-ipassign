bab5a67e11f3799a5a8e9554f3ef7c4520b48dc4
Cyril Danilevski <cydanil@gmail.com>
2020-04-08 19:30:41 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fin:
 
 setup(
      name='icepap-ipassign',
-     version='2.1.0',
+     version='2.2.0',
      author='Cyril Danilevski',
      author_email='cyril.danilevski@esrf.fr',
      description='A tool to configure IcePAP network settings',
